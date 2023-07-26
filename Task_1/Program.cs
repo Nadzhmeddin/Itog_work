a25e21212f69e327e69a0f2020c0f473259960c6
Nadzhmeddin <xside55@yandex.ru>
2023-07-27 00:51:28 +0300

    First method get Array Length

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -8,3 +8,16 @@
 string[] arrayMax = new string[10] {"Moscow", "Rome", "Str", "Int", "Hi", "Russia", "USA", "China", "Japan", "Day"};
 
 string[] arrayMini = new string[arrayMax.Length];
+
+void GetMiniArray(string[]arrayMax, string[]arrayMini)
+{
+    int count = 0;
+    for (int i = 0; i < arrayMax.Length; i++)
+    {
+        if(arrayMax[i].Length <= 3 )
+        {
+            arrayMini[count] = arrayMax[i];
+            count++;
+        }
+    }
+}
