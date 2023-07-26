aecbf2b93290b20f43f8e8b9c48a20b35cc7a551
Nadzhmeddin <xside55@yandex.ru>
2023-07-27 00:43:17 +0300

    New arrays

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+
+
+string[] arrayMax = new string[10] {"Moscow", "Rome", "Str", "Int", "Hi", "Russia", "USA", "China", "Japan", "Day"};
+
+string[] arrayMini = new string[arrayMax.Length];
